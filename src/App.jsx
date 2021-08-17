4944b9e96a436646a923c4acd9b8d1c43c66b891
Freya-Dixon <freya.dixon@hotmail.com>
2021-08-17 13:28:16 +0100

    added use effect statement to fetch punk api

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,31 @@ import HomePage from './components/HomePage'
 import NavBar from './components/NavBar'
 import BeerView from './components/BeerView'
 const App = () => {
+
+  const [beers, setBeers] = useState([])
+  const [inputValue, setInputValue] = useState('')
+ 
+
+  // display all beers on the page
+  useEffect(() => {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(res => res.json())
+    .then(data => { 
+      setBeers(data) 
+    console.log(data); 
+    })
+  }, []);
+
+  const handlechange = (e) => {
+    setInputValue(e.target.value)
+  }
+  // filter data with key of name to new input value
+  const filterBySearch = () => {
+    beers.filter(beer=> {
+      return beer.name.toLowerCase().includes(inputValue.toLowerCase())
+    })
+  }
+  
   return (
     <>
       <Router>
